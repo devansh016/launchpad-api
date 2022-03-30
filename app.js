e4bf29f3314901628eca49792e25ff172f0af9b0
Devansh Chaudhary <devanshchaudhary2002@gmail.com>
2022-03-30 23:04:51 +0530

    added database connectivity

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express')
 var app = express()
+var database = require('./utils/database')
 var port = process.env.PORT || 80
 
 
 //Starting Server
-app.listen(port, () =>{
+app.listen(port, () => {
 	console.log("App is running at port " + port)
 })
